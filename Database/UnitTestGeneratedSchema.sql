a16c94d324d8a081c742d17a02e8382427f57f73
Mike Hook <hook.mike@gmail.com>
2013-02-18 07:35:38 +0000

    First commit - Bare project.

---
+++ Database/UnitTestGeneratedSchema.sql
@@ -0,0 +1,8 @@
+
+    if exists (select * from dbo.sysobjects where id = object_id(N'CardTemplates') and OBJECTPROPERTY(id, N'IsUserTable') = 1) drop table CardTemplates
+
+    create table CardTemplates (
+        CardTemplateId INT IDENTITY NOT NULL,
+       CardName NVARCHAR(255) null,
+       primary key (CardTemplateId)
+    )
---
+++ Solutions/SunCow.Domain/CardTemplate.cs
@@ -0,0 +1,9 @@
+﻿namespace SunCow.Domain
+{
+    using SharpArch.Domain.DomainModel;
+
+    public class CardTemplate : Entity
+    {
+        public virtual string CardName { get; set; }
+    }
+}
